3128ef19ac5b2c5bd619e81723b4ecdb1316e0a1
Brent Roose <brent@spatie.be>
2021-07-07 09:00:00 +0200

    wip

--- tests/Models/BlogPostTest.php
+++ tests/Models/BlogPostTest.php
@@ -4,7 +4,6 @@ namespace Tests\Models;
 
 use App\Models\BlogPost;
 use App\Models\Enums\BlogPostStatus;
-use Carbon\Carbon;
 use Tests\TestCase;
 
 class BlogPostTest extends TestCase
@@ -16,13 +15,5 @@ class BlogPostTest extends TestCase
             'date' => '2021-06-01',
             'status' => BlogPostStatus::PUBLISHED(),
         ]);
-
-        $this->travelTo(Carbon::make('2021-01-01'));
-
-        $this->assertEquals(0, BlogPost::query()->wherePublished()->count());
-
-        $this->travelTo(Carbon::make('2021-06-01'));
-
-        $this->assertEquals(1, BlogPost::query()->wherePublished()->count());
     }
 }
