17ebacd10fcde11cbe0f8758f7be6856d89ee68b
Brent Roose <brent@spatie.be>
2021-07-13 09:22:14 +0200

    wip

--- tests/Policies/BlogPostPolicyTest.php
+++ tests/Policies/BlogPostPolicyTest.php
@@ -21,5 +21,31 @@ class BlogPostPolicyTest extends TestCase
                 ['is_admin' => true],
             )
             ->create();
+
+        $post = BlogPost::factory()->create();
+
+        $this->login($guest);
+
+        $this->get(action([BlogPostAdminController::class, 'index']))->assertForbidden();
+        $this->get(action([BlogPostAdminController::class, 'create']))->assertForbidden();
+        $this->post(action([BlogPostAdminController::class, 'store']))->assertForbidden();
+        $this->get(action([BlogPostAdminController::class, 'edit'], $post->slug))->assertForbidden();
+        $this->post(action([BlogPostAdminController::class, 'update'], $post->slug))->assertForbidden();
+        $this->post(action([BlogPostAdminController::class, 'publish'], $post->slug))->assertForbidden();
+        $this->post(action(UpdatePostSlugController::class, $post->slug))->assertForbidden();
+        $this->post(action(UpdatePostSlugController::class, $post->slug))->assertForbidden();
+        $this->post(action(DeletePostController::class, $post->slug))->assertForbidden();
+
+        $this->login($admin);
+
+        $this->get(action([BlogPostAdminController::class, 'index']))->assertSuccessful();
+        $this->get(action([BlogPostAdminController::class, 'create']))->assertSuccessful();
+        $this->post(action([BlogPostAdminController::class, 'store']))->assertRedirect();
+        $this->get(action([BlogPostAdminController::class, 'edit'], $post->slug))->assertSuccessful();
+        $this->post(action([BlogPostAdminController::class, 'update'], $post->slug))->assertRedirect();
+        $this->post(action([BlogPostAdminController::class, 'publish'], $post->slug))->assertRedirect();
+        $this->post(action(UpdatePostSlugController::class, $post->slug))->assertRedirect();
+        $this->post(action(UpdatePostSlugController::class, $post->slug))->assertRedirect();
+        $this->post(action(DeletePostController::class, $post->slug))->assertRedirect();
     }
 }
