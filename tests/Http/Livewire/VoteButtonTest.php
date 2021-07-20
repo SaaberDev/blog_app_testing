e012b712a13be1aeebaae0292a558d9cec01a942
Brent Roose <brent@spatie.be>
2021-07-20 09:56:42 +0200

    wip

--- tests/Http/Livewire/VoteButtonTest.php
+++ tests/Http/Livewire/VoteButtonTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Http\Livewire;
 
+use App\Http\Livewire\VoteButton;
 use App\Models\BlogPost;
+use App\Models\BlogPostLike;
+use Livewire\Livewire;
 use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 
@@ -16,5 +19,23 @@ class VoteButtonTest extends TestCase
         ]);
 
         $likerUuid = Uuid::uuid4();
+
+        $voteButton = Livewire::test(VoteButton::class, ['post' => $post, 'likerUuid' => $likerUuid]);
+
+        $voteButton->call('like');
+
+        $this->assertEquals(10 + 1, $post->refresh()->likes);
+        $this->assertDatabaseHas(BlogPostLike::class, [
+            'blog_post_id' => $post->id,
+            'liker_uuid' => $likerUuid,
+        ]);
+
+        $voteButton->call('like');
+
+        $this->assertEquals(10, $post->refresh()->likes);
+        $this->assertDatabaseMissing(BlogPostLike::class, [
+            'blog_post_id' => $post->id,
+            'liker_uuid' => $likerUuid,
+        ]);
     }
 }
