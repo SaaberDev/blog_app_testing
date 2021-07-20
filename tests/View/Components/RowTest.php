e91d4e27c423be474628cb608a83493bda496c1b
Brent Roose <brent@spatie.be>
2021-07-20 09:10:44 +0200

    wip

--- tests/View/Components/RowTest.php
+++ tests/View/Components/RowTest.php
@@ -9,5 +9,12 @@ class RowTest extends TestCase
     /** @test */
     public function header_row_is_rendered()
     {
+        $this->blade('<x-row header />')
+            ->assertSee('sticky')
+            ->assertSee('bg-gray');
+
+        $this->blade('<x-row />')
+            ->assertDontSee('sticky')
+            ->assertSee('bg-white');
     }
 }
