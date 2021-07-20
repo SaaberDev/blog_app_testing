4dcca861fa2a82b32b786a717448ab4f548f0c82
Brent Roose <brent@spatie.be>
2021-07-20 09:03:50 +0200

    wip

---
+++ tests/View/Components/LastSeenTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\View\Components;
+
+use Tests\TestCase;
+
+class LastSeenTest extends TestCase
+{
+    /** @test */
+    public function test_last_seen()
+    {
+    }
+}
---
+++ tests/View/Components/RowTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\View\Components;
+
+use Tests\TestCase;
+
+class RowTest extends TestCase
+{
+    /** @test */
+    public function header_row_is_rendered()
+    {
+    }
+}
