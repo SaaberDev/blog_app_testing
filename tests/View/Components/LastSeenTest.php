a99ec2a8ec48fee4402a9468cd3f2ec0cba7b2f2
Brent Roose <brent@spatie.be>
2021-07-20 09:15:01 +0200

    wip

--- tests/View/Components/LastSeenTest.php
+++ tests/View/Components/LastSeenTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\View\Components;
 
+use App\Models\BlogPost;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Tests\TestCase;
 
 class LastSeenTest extends TestCase
@@ -9,5 +12,18 @@ class LastSeenTest extends TestCase
     /** @test */
     public function test_last_seen()
     {
+        $post = BlogPost::factory()->create();
+
+        $this->travelTo(Carbon::make('2021-01-01'));
+
+        $this->blade('<x-last-seen :post="$post" />', ['post' => $post])
+            ->assertDontSee('Last seen')
+            ->assertDontSee('2021-01-01');
+
+        app(Request::class)->cookies->set("last_seen_{$post->slug}", now()->toDateTimeString());
+
+        $this->blade('<x-last-seen :post="$post" />', ['post' => $post])
+            ->assertSee('Last seen')
+            ->assertSee('2021-01-01');
     }
 }
