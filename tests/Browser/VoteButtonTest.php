47a9d1840a33aa56ff2fededca0eaeefd019a9c2
Brent Roose <brent@spatie.be>
2021-07-22 16:18:07 +0200

    wip

---
+++ tests/Browser/VoteButtonTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Http\Controllers\BlogPostController;
+use App\Models\BlogPost;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class VoteButtonTest extends DuskTestCase
+{
+    public function test_vote_button()
+    {
+        $post = BlogPost::factory()->create([
+            'likes' => 10,
+        ]);
+
+        $this->browse(function (Browser $browser) use ($post) {
+            $browser
+                ->visit(action([BlogPostController::class, 'show'], $post->slug))
+                ->with('.vote-button', function (Browser $button) {
+                    $button->assertSee('10');
+                })
+
+                ->click('.vote-button')
+                ->pause(1000)
+                ->with('.vote-button', function (Browser $button) {
+                    $button->assertSee('11');
+                })
+
+                ->click('.vote-button')
+                ->pause(1000)
+                ->with('.vote-button', function (Browser $button) {
+                    $button->assertSee('10');
+                })
+            ;
+        });
+    }
+}
