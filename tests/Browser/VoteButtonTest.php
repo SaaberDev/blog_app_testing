99dec7b7d85d9c8f55dfdddaeb002335373bd02f
Brent Roose <brent@spatie.be>
2021-07-27 13:49:44 +0200

    wip

--- tests/Browser/VoteButtonTest.php
+++ tests/Browser/VoteButtonTest.php
@@ -10,6 +10,8 @@ use Tests\DuskTestCase;
 
 class VoteButtonTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     public function test_vote_button()
     {
         $post = BlogPost::factory()->create([
@@ -35,6 +37,24 @@ class VoteButtonTest extends DuskTestCase
                     $button->assertSee('10');
                 })
             ;
+        });
+    }
+
+    public function test_vote_toggle()
+    {
+        $post = BlogPost::factory()->create([
+            'likes' => 10,
+        ]);
+
+        $this->browse(function (Browser $browser) use ($post) {
+            $browser
+                ->visit(action([BlogPostController::class, 'show'], $post->slug))
+                ->click('.vote-button')
+                ->pause(1000)
+                ->click('.vote-button')
+                ->pause(1000);
+
+            $this->assertEquals(10, $post->refresh()->likes);
         });
     }
 }
