5b5c28a9e740a43338ffd85520a6a57d699a074b
Brent Roose <brent@spatie.be>
2021-08-11 13:35:58 +0200

    wip

--- tests/Actions/SyncExternalPostTest.php
+++ tests/Actions/SyncExternalPostTest.php
@@ -16,25 +16,76 @@ class SyncExternalPostTest extends TestCase
     /** @test */
     public function synced_posts_are_stored_in_the_database()
     {
-        /** @var \App\Support\Rss\RssRepository $rss */
-        $rss = $this->mock(RssRepository::class, function (MockInterface $mock) {
-            $mock->shouldReceive('fetch')
-                ->andReturn(collect([
-                    new RssEntry(
-                        url: 'https://test.com',
-                        title: 'test',
-                        date: CarbonImmutable::make('2021-01-01'),
-                    )
-                ]));
-        });
+        Http::fake([
+            'https://test.com/*' => Http::sequence()
+                ->push($this->getFeed('test-a'))
+                ->push($this->getFeed('test-b')),
+        ]);
 
-        $sync = new SyncExternalPost($rss);
+        $sync = app(SyncExternalPost::class);
 
         $sync('https://test.com/feed');
 
         $this->assertDatabaseHas(ExternalPost::class, [
-            'url' => 'https://test.com',
-            'title' => 'test',
+            'url' => 'https://test.com/blog/test',
+            'title' => 'test-a',
+        ]);
+
+        $this->assertDatabaseMissing(ExternalPost::class, [
+            'url' => 'https://test.com/blog/test',
+            'title' => 'test-b',
+        ]);
+
+        $sync('https://test.com/feed');
+
+        $this->assertDatabaseHas(ExternalPost::class, [
+            'url' => 'https://test.com/blog/test',
+            'title' => 'test-b',
         ]);
     }
+
+    private function getFeed(string $title = 'test'): string
+    {
+        return  <<<XML
+       <feed xmlns="http://www.w3.org/2005/Atom">
+           <id>https://test.com/rss</id>
+           <link href="https://test.com/rss"/>
+           <title><![CDATA[ https://test.com ]]></title>
+           <updated>2021-08-11T11:00:30+00:00</updated>
+
+           <entry>
+               <title><![CDATA[$title]]></title>
+
+               <link rel="alternate" href="https://test.com/blog/test"/>
+
+               <id>https://test.com/blog/test</id>
+
+               <author>
+                   <name><![CDATA[ Brent Roose ]]></name>
+               </author>
+
+               <summary type="html"><![CDATA[$title]]></summary>
+
+               <updated>2021-07-29T00:00:00+00:00</updated>
+           </entry>
+
+           <entry>
+               <title><![CDATA[$title]]></title>
+
+               <link rel="alternate" href="https://test.com/blog/test"/>
+
+               <id>https://test.com/blog/test</id>
+
+               <author>
+                   <name><![CDATA[ Brent Roose ]]></name>
+               </author>
+
+               <summary type="html"><![CDATA[$title]]></summary>
+
+               <updated>2021-07-29T00:00:00+00:00</updated>
+           </entry>
+       </feed>
+       XML;
+    }
+
 }
